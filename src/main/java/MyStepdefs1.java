e12bf5a6fe772fadb9ef8f90aeaa51a2f2976228
Marcin <marcin-a94@wp.pl>
2019-08-25 14:49:19 +0200

    Initial commit

---
+++ src/main/java/MyStepdefs1.java
@@ -0,0 +1,28 @@
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MyStepdefs1 {
+    @Given("^User is on reservation page$")
+    public void userIsOnReservationPage() {
+    }
+
+    @When("^User chooses hotel offer$")
+    public void userChoosesHotelOffer() {
+    }
+
+    @Then("^User completes the reservation form$")
+    public void userCompletesTheReservationForm() {
+    }
+
+    @And("^Using a true information$")
+    public void usingATrueInformation() {
+    }
+
+    @And("^Reservation is confirmed$")
+    public void reservationIsConfirmed() {
+        
+    }
+
+
